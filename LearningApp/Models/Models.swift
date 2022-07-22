230dedd4e9362f10eae1453ae67c51a257d103a5
Patel Krupa <patelkrupa209@gmail.com>
2022-07-22 13:37:11 +1000

    Add data file and image
    parse style data and local data

---
+++ LearningApp/Models/Models.swift
@@ -0,0 +1,52 @@
+//
+//  Models.swift
+//  LearningApp
+//
+//  Created by Krupa Patel on 22/7/2022.
+//
+
+import Foundation
+
+struct Module: Decodable,Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+struct Content: Decodable,Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lessons]
+}
+
+struct Lessons: Decodable,Identifiable {
+    
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+}
+
+struct Test: Decodable,Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Questions]
+    
+}
+
+struct Questions: Decodable,Identifiable {
+    
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,5 +9,46 @@ import Foundation
 
 class ContentModel : ObservableObject {
     
+    @Published var modules = [Module]()
     
+    var styleData:Data?
+    
+    init() {
+        
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let decoder = JSONDecoder()
+            let modules = try decoder.decode([Module].self, from: jsonData)
+                
+                self.modules = modules
+                
+            } catch {
+                
+                print("Couldn't parse local data")
+            }
+       
+        // parse style Data
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+        } catch {
+            
+            print("coun't parse style Data")
+        }
+        
+    }
 }
